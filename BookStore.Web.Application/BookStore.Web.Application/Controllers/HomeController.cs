22b73406ee0a05c5dd93cef34f4d9fca182ed85c
SyedAmmarBukhari <ammarkust@gmail.com>
2020-08-21 11:26:48 +0500

    Configure MVC Services and Add Home Controller

---
+++ BookStore.Web.Application/BookStore.Web.Application/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Web.Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Index Page";
+        }
+    }
+}
